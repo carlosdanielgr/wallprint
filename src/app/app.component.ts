6a4ebdeac86dffef241900692d834c8e76d157e7
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-08-07 20:57:17 -0500

    first changes to title-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './components/header/header.component';
+import { TitleContentComponent } from './components/title-content/title-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TitleContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
