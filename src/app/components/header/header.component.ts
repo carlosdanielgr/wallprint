184b5d295d4c936b5741a0a8c0ab0293f4f5847e
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-08-07 19:33:28 -0500

    header component completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,24 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgbDropdownModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private readonly location: Location) {}
 
+  onInit() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+    this.location.replaceState('');
+  }
 }
