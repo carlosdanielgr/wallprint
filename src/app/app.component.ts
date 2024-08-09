855a01aab326c07a280ee52b261eb487baa45d3b
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-08-08 20:17:03 -0500

    our-work completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './components/header/header.component';
 import { TitleContentComponent } from './components/title-content/title-content.component';
+import { OurWorkComponent } from './components/our-work/our-work.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TitleContentComponent],
+  imports: [HeaderComponent, TitleContentComponent, OurWorkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
